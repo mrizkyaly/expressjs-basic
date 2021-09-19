ba80c7dd65c684dd8a501cbab1faf69ddb29bdc3
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-09-19 15:46:15 +0700

    first commit

---
+++ mongodb.js
@@ -0,0 +1,20 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(
+    'mongodb://localhost:27017',
+    { useUnifiedTopology: true },
+    function (err, client) {
+        if (err) throw err;
+        console.log('database berhasil connect');
+
+        var db = client.db('db_latihan');
+
+        db.collection('users')
+            .find()
+            .toArray(function (err, result) {
+                if (err) throw err;
+
+                console.log(result);
+            });
+    }
+);
